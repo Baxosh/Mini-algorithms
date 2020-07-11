1f1da59f396cf3211b9789f3075717537a1ea10d
Baxosh <63498656+Baxosh@users.noreply.github.com>
2020-07-11 09:37:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// console.log('--------------------');
+// for (var year = 2014; year <= 2050; year++) {
+//     for ( var day = 0; day <=6; day++) {
+//     var d = new Date(year, 3, day); 
+//     var datee = d.getDate(19);
+//     if ( d.getDate() == 19 )
+//         console.log(day);
+//     } 
+// }
+// console.log('--------------------'); 
